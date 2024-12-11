777dee459b67d33a8df51dd90e80b61bc0f1ab52
Arunkanakkillam <arungeo70@gmail.com>
2024-12-11 19:47:30 +0530

    initial

---
+++ Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Dapperbegin.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+    }
+
+}
